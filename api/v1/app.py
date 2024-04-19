248decaee219fcd8d31b0c9db0395b09f057367b
git-alexis <alex59.billemont@gmail.com>
2024-04-19 23:29:52 +0200

    test with getenv

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """It’s time to start your API"""
 
-from flask import Flask, jsonify
+from flask import jsonify
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
-
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
@@ -27,4 +26,6 @@ def remove_session(exception):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
