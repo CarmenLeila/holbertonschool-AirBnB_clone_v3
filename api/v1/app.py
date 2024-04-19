b79602f23f7d968e10f8f6d6006a58d03c9cfd80
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-04-19 21:58:10 +0200

    adding a necessary line for env variable

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
